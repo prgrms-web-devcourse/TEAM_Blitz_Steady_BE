079a6760f0999dddf2cadee27bc7c176e0c666ff
na-yk <ninghao17@naver.com>
2023-10-23 18:49:00 +0900

    :sparkles: UserRepository 추가

--- src/main/java/dev/steady/user/infrastructure/UserRepository.java
+++ src/main/java/dev/steady/user/infrastructure/UserRepository.java
@@ -3,11 +3,8 @@ package dev.steady.user.infrastructure;
 import dev.steady.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.List;
 
-    default User getUserBy(Long userId) {
-        return findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException());
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
