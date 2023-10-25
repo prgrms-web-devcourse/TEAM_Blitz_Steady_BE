b6d7652ef33ed97a6329f33752e4e783f4091f96
jun <alap_u@naver.com>
2023-10-25 17:11:14 +0900

    :sparkles: 템플릿 양식 도메인 생성

---
+++ src/main/java/dev/steady/template/domain/Question.java
@@ -0,0 +1,36 @@
+package dev.steady.template.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "template_id")
+    private Template template;
+
+    @Column(nullable = false)
+    private String title;
+
+    public Question(Template template, String title) {
+        this.template = template;
+        this.title = title;
+    }
+
+}
