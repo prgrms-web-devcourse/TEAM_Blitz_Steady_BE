e1e282c46e2f42d9d4a8636133754e39d2124509
weonest <geonhee33@naver.com>
2023-10-25 16:53:56 +0900

    :recycle: 기존의 ERD 변경에 따른 Steady 도메인 수정 및 생성 로직 변경

--- src/main/java/dev/steady/steady/domain/Promotion.java
+++ src/main/java/dev/steady/steady/domain/Promotion.java
@@ -2,30 +2,25 @@ package dev.steady.steady.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.EntityListeners;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Embeddable
-@EntityListeners(AuditingEntityListener.class)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Promotion {
+
+    private final static int INITIAL_PROMOTION_COUNT = 3;
 
     @Column(name = "promotion_count", nullable = false)
     private int promotion_count;
 
-    @CreatedDate
     @Column(name = "promoted_at", nullable = false)
     private LocalDateTime promoted_at;
 
-    public Promotion(int promotion_count) {
-        this.promotion_count = promotion_count;
+    public Promotion() {
+        this.promotion_count = INITIAL_PROMOTION_COUNT;
+        this.promoted_at = LocalDateTime.now();
     }
 
 }
--- src/test/java/dev/steady/steady/service/SteadyServiceTest.java
+++ src/test/java/dev/steady/steady/service/SteadyServiceTest.java
@@ -1,14 +1,13 @@
-package dev.steady.steady.application;
+package dev.steady.steady.service;
 
-import dev.steady.steadyForm.domain.SteadyForm;
-import dev.steady.steadyForm.fixture.SteadyFormFixtures;
-import dev.steady.steadyForm.infrastructure.SteadyFormRepository;
 import dev.steady.global.auth.AuthContext;
 import dev.steady.steady.domain.Promotion;
 import dev.steady.steady.domain.Steady;
+import dev.steady.steady.domain.SteadyQuestion;
+import dev.steady.steady.domain.repository.SteadyQuestionRepository;
 import dev.steady.steady.dto.request.SteadyCreateRequest;
 import dev.steady.steady.fixture.SteadyFixtures;
-import dev.steady.steady.infrastructure.SteadyRepository;
+import dev.steady.steady.domain.repository.SteadyRepository;
 import dev.steady.user.domain.User;
 import dev.steady.user.fixture.UserFixtures;
 import dev.steady.user.infrastructure.UserRepository;
@@ -19,11 +18,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SteadyServiceTest {
@@ -38,22 +40,21 @@ class SteadyServiceTest {
     private UserRepository userRepository;
 
     @Mock
-    private SteadyFormRepository steadyFormRepository;
+    private SteadyQuestionRepository steadyQuestionRepository;
 
     @Mock
     private AuthContext authContext;
 
     @Test
-    @DisplayName("스터디 생성 요청을 통해 스터디를 생성할 수 있다")
+    @DisplayName("스터디 생성 요청을 통해 스터디와 스테디 질문을 생성할 수 있다")
     void createSteadyTest() {
         // given
         User user = UserFixtures.createUser();
         SteadyCreateRequest steadyRequest = SteadyFixtures.createSteadyRequest();
-        SteadyForm steadyForm = SteadyFormFixtures.createForm(user);
         Promotion promotion = SteadyFixtures.createPromotion();
-        Steady steady = SteadyFixtures.createSteady(steadyRequest, promotion, steadyForm);
+        Steady steady = SteadyFixtures.createSteady(steadyRequest, promotion);
+        List<SteadyQuestion> steadyQuestions = SteadyFixtures.createSteadyQuestions(steadyRequest.questionList(), steady);
 
-        given(steadyFormRepository.findById(any())).willReturn(Optional.ofNullable(steadyForm));
         given(authContext.getUserId()).willReturn(user.getId());
         given(userRepository.findById(any())).willReturn(Optional.ofNullable(user));
         given(steadyRepository.save(any())).willReturn(steady);
@@ -63,6 +64,8 @@ class SteadyServiceTest {
 
         // then
         assertThat(returnedId).isEqualTo(steady.getId());
+        then(steadyQuestionRepository).should(atMostOnce()).saveAll(any());
+        // steadyQuestionRepository의 동작을 given에서 처리하면 불필요한 stubbing 동작이라는 예외
     }
 
 }
