c307e2dbfbe631072b23ab68af5e4c66334ec1b6
jun <alap_u@naver.com>
2023-10-27 03:13:59 +0900

    :white_check_mark: 템플릿 목록 조회 테스트 코드 작성
    컨트롤러 테스트 ToDo 주석 작성

--- src/test/java/dev/steady/template/service/TemplateServiceTest.java
+++ src/test/java/dev/steady/template/service/TemplateServiceTest.java
@@ -1,11 +1,13 @@
 package dev.steady.template.service;
 
 import dev.steady.global.auth.AuthContext;
+import dev.steady.global.auth.AuthFixture;
 import dev.steady.template.domain.Question;
 import dev.steady.template.domain.Template;
 import dev.steady.template.domain.repository.QuestionRepository;
 import dev.steady.template.domain.repository.TemplateRepository;
 import dev.steady.template.dto.request.CreateTemplateRequest;
+import dev.steady.template.dto.resonse.TemplateResponses;
 import dev.steady.user.domain.User;
 import dev.steady.user.fixture.UserFixtures;
 import dev.steady.user.infrastructure.UserRepository;
@@ -17,6 +19,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static dev.steady.template.fixture.TemplateFixture.createAnotherTemplate;
+import static dev.steady.template.fixture.TemplateFixture.createTemplate;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -61,6 +65,24 @@ class TemplateServiceTest {
                 () -> assertThat(savedQuestions.size()).isEqualTo(savedQuestions.size()),
                 () -> assertThat(template).isNotNull()
         );
+    }
+
+    @DisplayName("사용자의 계정 정보를 받아 템플릿 목록을 조회한다.")
+    @Test
+    void getTemplatesTest(){
+        var user = UserFixtures.createUser();
+        var savedUser = userRepository.save(user);
+
+        var template1 = createTemplate(savedUser);
+        var template2 = createAnotherTemplate(savedUser);
+        templateRepository.saveAll(List.of(template1, template2));
+
+        var authContext = AuthFixture.createAuthContext(savedUser.getId());
+        TemplateResponses templates = templateService.getTemplates(authContext);
+
+        assertThat(templates.templates()).hasSize(2)
+                .extracting("title")
+                .containsExactlyInAnyOrder(template1.getTitle(), template2.getTitle());
     }
 
 }
