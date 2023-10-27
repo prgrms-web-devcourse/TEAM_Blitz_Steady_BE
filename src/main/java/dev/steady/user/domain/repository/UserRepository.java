2d5250de02d68514ee705b26c17fb490e0e26c79
na-yk <ninghao17@naver.com>
2023-10-27 16:52:54 +0900

    :truck: UserRepository 패키지 이동

---
+++ src/main/java/dev/steady/user/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package dev.steady.user.domain.repository;
+
+import dev.steady.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    default User getUserBy(Long userId) {
+        return findById(userId)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+}
