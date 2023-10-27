2571f3872f1ec615d9f5e321fa90409d48d9802e
jun <alap_u@naver.com>
2023-10-27 18:49:03 +0900

    :white_check_mark: 템플릿 작성자 유효성 검증테스트

---
+++ src/test/java/dev/steady/template/domain/TemplateTest.java
@@ -0,0 +1,23 @@
+package dev.steady.template.domain;
+
+import dev.steady.template.fixture.TemplateFixture;
+import dev.steady.user.domain.User;
+import dev.steady.user.fixture.UserFixtures;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class TemplateTest {
+
+    @DisplayName("템플릿의 생성자가 아니면 예외가 발생한다.")
+    @Test
+    void validateOwnerTest() {
+        User user = UserFixtures.createUser();
+        Template template = TemplateFixture.createTemplate(user);
+
+        assertThatThrownBy(() -> template.validateOwner(UserFixtures.createAnotherUser()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
