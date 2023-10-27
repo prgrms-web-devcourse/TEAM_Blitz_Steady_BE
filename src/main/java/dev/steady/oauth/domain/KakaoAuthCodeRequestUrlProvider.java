103202058a24756083c970d44ed8a477314deef4
na-yk <ninghao17@naver.com>
2023-10-27 16:22:38 +0900

    :sparkles: OAuth 인가코드 요청 URL provider 추가

---
+++ src/main/java/dev/steady/oauth/domain/KakaoAuthCodeRequestUrlProvider.java
@@ -0,0 +1,30 @@
+package dev.steady.oauth.domain;
+
+import dev.steady.oauth.config.KakaoOAuthProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@RequiredArgsConstructor
+public class KakaoAuthCodeRequestUrlProvider implements AuthCodeRequestUrlProvider {
+
+    private static final String AUTHORIZATION_URL = "https://kauth.kakao.com/oauth/authorize";
+    private final KakaoOAuthProperties kakaoOAuthProperties;
+
+    @Override
+    public Platform platform() {
+        return Platform.KAKAO;
+    }
+
+    @Override
+    public String provideUrl() {
+        return UriComponentsBuilder
+                .fromUriString(AUTHORIZATION_URL)
+                .queryParam("client_id", kakaoOAuthProperties.getClientId())
+                .queryParam("redirect_uri", kakaoOAuthProperties.getRedirectUri())
+                .queryParam("response_type", kakaoOAuthProperties.getResponseType())
+                .toUriString();
+    }
+
+}
