d8789fa245473e9dfceb03359141446d3d85b92b
na-yk <ninghao17@naver.com>
2023-10-27 16:59:54 +0900

    :sparkles: Account 등록 기능 추가

--- src/main/java/dev/steady/oauth/service/OAuthService.java
+++ src/main/java/dev/steady/oauth/service/OAuthService.java
@@ -9,9 +9,33 @@ import org.springframework.stereotype.Service;
 public class OAuthService {
 
     private final AuthCodeRequestService authCodeRequestService;
+    private final OAuthClientService oAuthClientService;
+    private final AccountService accountService;
 
     public String getAuthCodeRequestUrlProvider(Platform platform) {
         return authCodeRequestService.provideRequestUrl(platform);
+    }
+
+    public LogInResponse logIn(Platform platform, String authCode) {
+        OAuthUserInfoResponse userInfo = oAuthClientService.getUserInfo(platform, authCode);
+
+        try {
+            AccountResponse accountResponse = accountService.findByPlatformAndPlatformId(userInfo.getPlatform(), userInfo.getPlatformId());
+            if (accountResponse.user() == null) {
+                return LogInResponse.of(accountResponse.id(), true);
+            } else {
+                // TODO: 2023-10-25 서비스 JWT 발급 로직 추가
+                return null;
+            }
+        } catch (Exception e) {
+            Long accountId = signUp(userInfo);
+            return LogInResponse.of(accountId, true);
+        }
+    }
+
+    public Long signUp(OAuthUserInfoResponse userInfo) {
+        AccountCreateRequest request = AccountCreateRequest.from(userInfo);
+        return accountService.create(request);
     }
 
 }
