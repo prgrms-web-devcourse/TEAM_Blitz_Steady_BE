dc3755485ecd41df46ff20e3442ab09d7653d49f
na-yk <ninghao17@naver.com>
2023-10-27 17:06:18 +0900

    :sparkles: Access Token 발급 로직 추가

---
+++ src/main/java/dev/steady/oauth/config/JwtProperties.java
@@ -0,0 +1,14 @@
+package dev.steady.oauth.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    private final String secretKey;
+    private final Long accessTokenExpiration;
+    private final Long refreshTokenExpiration;
+}
--- src/main/java/dev/steady/oauth/service/OAuthService.java
+++ src/main/java/dev/steady/oauth/service/OAuthService.java
@@ -1,6 +1,14 @@
 package dev.steady.oauth.service;
 
+import dev.steady.auth.domain.JwtProvider;
+import dev.steady.auth.dto.request.AccountCreateRequest;
+import dev.steady.auth.dto.request.TokenRequest;
+import dev.steady.auth.dto.response.AccountResponse;
+import dev.steady.auth.dto.response.TokenResponse;
+import dev.steady.auth.service.AccountService;
 import dev.steady.oauth.domain.Platform;
+import dev.steady.oauth.dto.response.LogInResponse;
+import dev.steady.oauth.dto.response.OAuthUserInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +19,7 @@ public class OAuthService {
     private final AuthCodeRequestService authCodeRequestService;
     private final OAuthClientService oAuthClientService;
     private final AccountService accountService;
+    private final JwtProvider jwtProvider;
 
     public String getAuthCodeRequestUrlProvider(Platform platform) {
         return authCodeRequestService.provideRequestUrl(platform);
@@ -24,8 +33,13 @@ public class OAuthService {
             if (accountResponse.user() == null) {
                 return LogInResponse.of(accountResponse.id(), true);
             } else {
-                // TODO: 2023-10-25 서비스 JWT 발급 로직 추가
-                return null;
+                TokenRequest tokenRequest = TokenRequest.from(accountResponse);
+                String accessToken = jwtProvider.provideAccessToken(tokenRequest);
+
+                // TODO: 2023-10-26 리프레시 토큰 생성 로직 추가
+
+                TokenResponse token = TokenResponse.of(accessToken, "리프레시 토큰");
+                return LogInResponse.of(accountResponse.id(), false, token);
             }
         } catch (Exception e) {
             Long accountId = signUp(userInfo);
