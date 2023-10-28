feefef7ae2200d561cf076f4b871d5bb2292ac91
na-yk <ninghao17@naver.com>
2023-10-28 22:03:46 +0900

    :recycle: OAuthService가 AccountService를 의존하지 않도록 개선

--- src/main/java/dev/steady/auth/service/AccountService.java
+++ src/main/java/dev/steady/auth/service/AccountService.java
@@ -2,9 +2,6 @@ package dev.steady.auth.service;
 
 import dev.steady.auth.domain.Account;
 import dev.steady.auth.domain.repository.AccountRepository;
-import dev.steady.auth.dto.request.AccountCreateRequest;
-import dev.steady.auth.dto.response.AccountResponse;
-import dev.steady.oauth.domain.Platform;
 import dev.steady.user.domain.User;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -18,23 +15,9 @@ public class AccountService {
     private final AccountRepository accountRepository;
 
     @Transactional
-    public Long create(AccountCreateRequest request) {
-        Account account = AccountCreateRequest.toEntity(request.platform(), request.platformId());
-        Account saved = accountRepository.save(account);
-
-        return saved.getId();
-    }
-
-    public AccountResponse findByPlatformAndPlatformId(Platform platform, String platformId) {
-        Account account = accountRepository.findByPlatformAndPlatformId(platform, platformId)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("플랫폼 %s의 id %s에 해당하는 계정이 없습니다.", platform.name(), platformId)));
-        return AccountResponse.from(account);
-    }
-
-    @Transactional
     public void registUser(Long accountId, User user) {
         Account account = accountRepository.findById(accountId)
-                .orElseThrow(() -> new EntityNotFoundException("일치하는 계정이 없습니다."));
+                .orElseThrow(() -> new EntityNotFoundException(String.format("일치하는 계정이 없습니다: %d", accountId)));
         account.registUser(user);
     }
 
