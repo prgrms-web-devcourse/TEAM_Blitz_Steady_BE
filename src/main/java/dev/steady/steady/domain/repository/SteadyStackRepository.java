9050a501f5b7ca974fb119657042539f0d2fb46d
weonest <geonhee33@naver.com>
2023-10-29 15:56:20 +0900

    :white_check_mark: steady 조회 로직 추가

--- src/main/java/dev/steady/steady/domain/repository/SteadyStackRepository.java
+++ src/main/java/dev/steady/steady/domain/repository/SteadyStackRepository.java
@@ -3,5 +3,10 @@ package dev.steady.steady.domain.repository;
 import dev.steady.steady.domain.SteadyStack;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SteadyStackRepository extends JpaRepository<SteadyStack, Long> {
+
+    List<SteadyStack> findBySteadyId(Long id);
+
 }
