2152cd38dc956f25ba8520905533fef88eafcaa9
weonest <geonhee33@naver.com>
2023-10-29 20:41:29 +0900

    :white_check_mark: 스테디 상제 조회 로직 추가

--- src/main/java/dev/steady/steady/presentation/SteadyController.java
+++ src/main/java/dev/steady/steady/presentation/SteadyController.java
@@ -4,11 +4,13 @@ import dev.steady.global.auth.AuthContext;
 import dev.steady.steady.dto.request.SteadyCreateRequest;
 import dev.steady.steady.dto.request.SteadyPageRequest;
 import dev.steady.steady.dto.response.PageResponse;
+import dev.steady.steady.dto.response.SteadyDetailResponse;
 import dev.steady.steady.dto.response.SteadySearchResponse;
 import dev.steady.steady.service.SteadyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +23,10 @@ import java.net.URI;
 @RequestMapping("/api/v1/steadies")
 public class SteadyController {
 
-    private final AuthContext authContext;
     private final SteadyService steadyService;
 
     @PostMapping
-    public ResponseEntity<Void> createSteady(@RequestBody SteadyCreateRequest request) {
+    public ResponseEntity<Void> createSteady(@RequestBody SteadyCreateRequest request, AuthContext authContext) {
         Long steadyId = steadyService.create(request, authContext);
         return ResponseEntity.created(URI.create(String.format("/api/v1/steadies/%d/detail", steadyId))).build();
     }
@@ -33,6 +34,14 @@ public class SteadyController {
     @GetMapping
     public ResponseEntity<PageResponse<SteadySearchResponse>> getSteadiesPage(@RequestBody SteadyPageRequest request) {
         PageResponse<SteadySearchResponse> response = steadyService.getSteadies(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{steadyId}")
+    public ResponseEntity<SteadyDetailResponse> getDetailSteady(@PathVariable Long steadyId,
+                                                                @RequestBody SteadyPageRequest request,
+                                                                AuthContext authContext) {
+        SteadyDetailResponse response = steadyService.getDetailSteady(steadyId, authContext);
         return ResponseEntity.ok(response);
     }
 
