5a2600025122c0891c1c7f25773e3417d5b0e8ed
na-yk <ninghao17@naver.com>
2023-10-29 15:59:18 +0900

    :sparkles: JwtProperties Configuration 추가

--- src/main/java/dev/steady/global/config/PropertiesConfig.java
+++ src/main/java/dev/steady/global/config/PropertiesConfig.java
@@ -1,11 +1,12 @@
 package dev.steady.global.config;
 
+import dev.steady.oauth.config.JwtProperties;
 import dev.steady.oauth.config.KakaoOAuthProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(value = {KakaoOAuthProperties.class})
+@EnableConfigurationProperties(value = {KakaoOAuthProperties.class, JwtProperties.class})
 public class PropertiesConfig {
 
 }
