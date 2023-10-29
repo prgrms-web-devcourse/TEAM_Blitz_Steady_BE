076f12699d3b54ef6a3449960d566038da3fbf53
na-yk <ninghao17@naver.com>
2023-10-29 18:57:49 +0900

    :sparkles: User Position 설정 로직 추가

--- src/main/java/dev/steady/user/dto/request/UserCreateRequest.java
+++ src/main/java/dev/steady/user/dto/request/UserCreateRequest.java
@@ -1,16 +1,19 @@
 package dev.steady.user.dto.request;
 
+import dev.steady.user.domain.Position;
 import dev.steady.user.domain.User;
 
 public record UserCreateRequest(
-        String nickname
+        String nickname,
+        Long positionId
 ) {
 
-    public static User toEntity(UserCreateRequest request) {
-        // TODO: 2023-10-26 스택, 포지션 정보 저장 로직 추가
+    public User toEntity(Position position) {
+        // TODO: 2023-10-29 스택 정보 저장 로직 추가
         return User.builder()
-                .nickname(request.nickname())
+                .nickname(this.nickname)
                 .profileImage("기본 이미지")
+                .position(position)
                 .build();
     }
 
--- src/main/java/dev/steady/user/service/UserService.java
+++ src/main/java/dev/steady/user/service/UserService.java
@@ -1,9 +1,12 @@
 package dev.steady.user.service;
 
 import dev.steady.auth.service.AccountService;
+import dev.steady.user.domain.Position;
 import dev.steady.user.domain.User;
+import dev.steady.user.domain.repository.PositionRepository;
 import dev.steady.user.domain.repository.UserRepository;
 import dev.steady.user.dto.request.UserCreateRequest;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +17,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PositionRepository positionRepository;
     private final AccountService accountService;
 
     @Transactional
     public Long createUser(Long accountId, UserCreateRequest request) {
-        User user = UserCreateRequest.toEntity(request);
+        Position position = positionRepository.findById(request.positionId())
+                .orElseThrow(() -> new EntityNotFoundException(String.format("해당하는 포지션이 없습니다:%d", request.positionId())));
+
+        User user = request.toEntity(position);
         User saved = userRepository.save(user);
         accountService.registUser(accountId, saved);
         return saved.getId();
