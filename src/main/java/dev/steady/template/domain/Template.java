78a8f5d44085735315e0e65edf6e1fc44e3a0a52
weonest <geonhee33@naver.com>
2023-10-29 14:57:43 +0900

    :lipstick: import문 컨벤션 맞춰서 수정

--- src/main/java/dev/steady/template/domain/Template.java
+++ src/main/java/dev/steady/template/domain/Template.java
@@ -2,7 +2,16 @@ package dev.steady.template.domain;
 
 import dev.steady.global.entity.BaseEntity;
 import dev.steady.user.domain.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
