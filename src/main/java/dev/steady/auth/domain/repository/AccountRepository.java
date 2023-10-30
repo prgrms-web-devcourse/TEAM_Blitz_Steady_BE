96c5da7fee53f71ad0eef8a2816728ecf3feb69a
na-yk <ninghao17@naver.com>
2023-10-30 16:12:06 +0900

    :sparkles: AccountRepository 추가

---
+++ src/main/java/dev/steady/auth/domain/repository/AccountRepository.java
@@ -0,0 +1,15 @@
+package dev.steady.auth.domain.repository;
+
+import dev.steady.auth.domain.Account;
+import dev.steady.auth.domain.Platform;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    boolean existsByPlatformAndPlatformId(Platform platform, String platformId);
+
+    Optional<Account> findByPlatformAndPlatformId(Platform platform, String platformId);
+
+}
