c6746594dc65a8d1d5f949eff5f180f0ddd6d4e4
na-yk <ninghao17@naver.com>
2023-10-30 15:52:48 +0900

    :sparkles: Account 엔티티 추가

---
+++ src/main/java/dev/steady/auth/domain/Account.java
@@ -0,0 +1,47 @@
+package dev.steady.auth.domain;
+
+import dev.steady.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "accounts")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "platform", nullable = false)
+    private Platform platform;
+
+    @Column(name = "platform_id", nullable = false)
+    private String platformId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Builder
+    private Account(String platformId, Platform platform) {
+        this.platformId = platformId;
+        this.platform = platform;
+    }
+
+}
