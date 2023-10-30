27bf8f4903248af91724762bb1e22b13805f11e8
na-yk <ninghao17@naver.com>
2023-10-30 16:04:19 +0900

    :sparkles: Kakao OAuth AccessToken 발급 client 추가

---
+++ src/main/java/dev/steady/auth/oauth/client/KakaoOAuthClient.java
@@ -0,0 +1,54 @@
+package dev.steady.auth.oauth.client;
+
+import dev.steady.auth.config.KakaoOAuthProperties;
+import dev.steady.auth.domain.Platform;
+import dev.steady.auth.oauth.dto.KakaoToken;
+import dev.steady.auth.oauth.dto.response.KakaoUserInfoResponse;
+import dev.steady.auth.oauth.dto.response.OAuthUserInfoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@RequiredArgsConstructor
+public class KakaoOAuthClient implements OAuthClient {
+
+    private static final String GRANT_TYPE = "authorization_code";
+    private static final String REQUEST_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
+    private static final String REQUEST_USER_INFO_URL = "https://kapi.kakao.com/v2/user/me";
+    private final KakaoOAuthProperties kakaoOAuthProperties;
+    private final RestTemplate restTemplate;
+
+    @Override
+    public Platform getPlatform() {
+        return Platform.KAKAO;
+    }
+
+    @Override
+    public String getAccessToken(String authCode) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", GRANT_TYPE);
+        body.add("client_id", kakaoOAuthProperties.getClientId());
+        body.add("redirect_uri", kakaoOAuthProperties.getRedirectUri());
+        body.add("client_secret", kakaoOAuthProperties.getClientSecret());
+        body.add("code", authCode);
+
+        KakaoToken token = restTemplate.postForObject(
+                REQUEST_TOKEN_URL,
+                new HttpEntity<>(body, httpHeaders),
+                KakaoToken.class
+        );
+
+        if (token == null) throw new AssertionError("토큰을 발급 받을 수 없습니다.");
+        return token.accessToken();
+    }
+
+}
---
+++ src/main/java/dev/steady/auth/oauth/service/OAuthClientService.java
@@ -0,0 +1,26 @@
+package dev.steady.auth.oauth.service;
+
+import dev.steady.auth.domain.Platform;
+import dev.steady.auth.oauth.client.OAuthClient;
+import dev.steady.auth.oauth.dto.response.OAuthUserInfoResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+public class OAuthClientService {
+
+    private final Map<Platform, OAuthClient> clientMap;
+
+    public OAuthClientService(Set<OAuthClient> clients) {
+        this.clientMap = clients.stream()
+                .collect(Collectors.toUnmodifiableMap(
+                        OAuthClient::getPlatform,
+                        Function.identity()
+                ));
+    }
+
+}
