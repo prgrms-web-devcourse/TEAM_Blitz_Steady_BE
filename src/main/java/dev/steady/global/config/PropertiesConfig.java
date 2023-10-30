2d9d5feb45c3f9959d140cfb5b353d70d4ea011e
na-yk <ninghao17@naver.com>
2023-10-30 16:10:44 +0900

    :sparkles: JWT 프로퍼티 바인딩

--- src/main/java/dev/steady/global/config/PropertiesConfig.java
+++ src/main/java/dev/steady/global/config/PropertiesConfig.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(value = {KakaoOAuthProperties.class})
+@EnableConfigurationProperties(value = {KakaoOAuthProperties.class, JwtProperties.class})
 public class PropertiesConfig {
 
 }
