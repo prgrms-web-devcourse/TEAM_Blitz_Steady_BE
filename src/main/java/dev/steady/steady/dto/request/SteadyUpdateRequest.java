0c8dbceb10fc58832d4d440ee3ce564927726db0
weonest <geonhee33@naver.com>
2023-10-30 22:07:18 +0900

    :white_check_mark: SteadyUpdateRequest 추가

---
+++ src/main/java/dev/steady/steady/dto/request/SteadyUpdateRequest.java
@@ -0,0 +1,23 @@
+package dev.steady.steady.dto.request;
+
+import dev.steady.steady.domain.SteadyMode;
+import dev.steady.steady.domain.SteadyType;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record SteadyUpdateRequest(
+        String name,
+        String bio,
+        SteadyType type,
+        int participantLimit,
+        SteadyMode steadyMode,
+        LocalDate openingDate,
+        LocalDate deadline,
+        String title,
+        String content,
+        List<Long> positions,
+        List<Long> stacks
+) {
+
+}
