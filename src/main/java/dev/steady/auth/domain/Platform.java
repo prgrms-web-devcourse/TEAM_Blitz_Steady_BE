ea10fa1dd6e964e7342612c9a6d8d5b333c88dcc
na-yk <ninghao17@naver.com>
2023-10-30 15:52:59 +0900

    :truck: Platform 패키지 이동

---
+++ src/main/java/dev/steady/auth/domain/Platform.java
@@ -0,0 +1,17 @@
+package dev.steady.auth.domain;
+
+import java.util.Arrays;
+
+public enum Platform {
+    KAKAO,
+    NAVER,
+    GOOGLE;
+
+    public static Platform from(String platformName) {
+        return Arrays.stream(Platform.values())
+                .filter(v -> v.name().equals(platformName.toUpperCase()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("일치하는 플랫폼이 없습니다: %s", platformName)));
+    }
+
+}
--- src/main/java/dev/steady/user/domain/Platform.java
+++
@@ -1,9 +0,0 @@
-package dev.steady.user.domain;
-
-public enum Platform {
-
-    KAKAO,
-    NAVER,
-    GOOGLE
-
-}
