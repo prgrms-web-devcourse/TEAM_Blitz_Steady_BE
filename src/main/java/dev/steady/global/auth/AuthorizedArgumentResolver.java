042316854930ced27c42d24589cea2407752fc82
weonest <geonhee33@naver.com>
2023-10-30 16:07:31 +0900

    :white_check_mark: UserInfo를 가져올 수 있도록 ArgumentResolver 추가

---
+++ src/main/java/dev/steady/global/auth/AuthorizedArgumentResolver.java
@@ -0,0 +1,34 @@
+package dev.steady.global.auth;
+
+import dev.steady.user.infrastructure.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class AuthorizedArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final AuthContext authContext;
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(Auth.class) != null
+        && parameter.getParameterType().equals(UserInfo.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        UserInfo userInfo = UserInfo.from(authContext);
+        if (userRepository.existsById(userInfo.userId()))
+            return userInfo;
+        throw new IllegalArgumentException();
+    }
+
+}
