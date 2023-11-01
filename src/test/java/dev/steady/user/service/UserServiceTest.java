f187c4ed3edaf86f009afc6b3b3c52f40997d59b
na-yk <ninghao17@naver.com>
2023-11-01 11:35:12 +0900

    :white_check_mark: UserService createUser 테스트 추가

---
+++ src/test/java/dev/steady/user/service/UserServiceTest.java
@@ -0,0 +1,62 @@
+package dev.steady.user.service;
+
+import dev.steady.user.domain.Stack;
+import dev.steady.user.domain.repository.PositionRepository;
+import dev.steady.user.domain.repository.StackRepository;
+import dev.steady.user.domain.repository.UserRepository;
+import dev.steady.user.domain.repository.UserStackRepository;
+import dev.steady.user.dto.request.CreateUserRequest;
+import dev.steady.user.fixture.UserFixtures;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Transactional
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private StackRepository stackRepository;
+    @Autowired
+    private PositionRepository positionRepository;
+    @Autowired
+    private UserStackRepository userStackRepository;
+
+    @DisplayName("프로필 정보를 입력받아 유저를 생성한다.")
+    @Test
+    void createUser() {
+        // given
+        var savedPosition = positionRepository.save(UserFixtures.createPosition());
+        var savedStacks = stackRepository.saveAll(
+                IntStream.range(0, 3)
+                        .mapToObj(i -> UserFixtures.createStack())
+                        .toList());
+        var stackIds = savedStacks.stream().map(Stack::getId).toList();
+
+        // when
+        var request = new CreateUserRequest(1L, "Nickname", savedPosition.getId(), stackIds);
+        var userId = userService.createUser(request);
+
+        var userStacks = userStackRepository.findAllByUserId(userId);
+
+        // then
+        var user = userRepository.findById(userId).get();
+        assertAll(
+                () -> assertThat(user.getId()).isEqualTo(userId),
+                () -> assertThat(user.getPosition()).isEqualTo(savedPosition),
+                () -> assertThat(userStacks).hasSameSizeAs(request.stackIds())
+        );
+    }
+
+}
