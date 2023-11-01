feedf385fe8fbeddbf0530300e510f9a79c582ce
jun <alap_u@naver.com>
2023-11-02 01:00:43 +0900

    :sparkles: 도메인재정의 및 양방향 관계 추가

--- src/main/java/dev/steady/application/domain/Application.java
+++ src/main/java/dev/steady/application/domain/Application.java
@@ -2,7 +2,7 @@ package dev.steady.application.domain;
 
 import dev.steady.steady.domain.Steady;
 import dev.steady.user.domain.User;
-import jakarta.persistence.Column;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -12,10 +12,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static dev.steady.application.domain.ApplicationStatus.WAITING;
 
 @Entity
 @Getter
@@ -35,15 +41,21 @@ public class Application {
     @JoinColumn(name = "steady_id")
     private Steady steady;
 
-    @Column
+    @OneToMany(mappedBy = "application", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<SurveyResult> surveyResults = new ArrayList<>();
+
     @Enumerated(EnumType.STRING)
     private ApplicationStatus status;
 
     public Application(User user, Steady steady) {
         this.user = user;
         this.steady = steady;
-        this.status = ApplicationStatus.WAITING;
+        this.status = WAITING;
     }
 
+    public void addSurveyResult(SurveyResult surveyResult) {
+        surveyResult.setApplication(this);
+        surveyResults.add(surveyResult);
+    }
 
 }
---
+++ src/main/java/dev/steady/application/domain/SurveyResult.java
@@ -0,0 +1,60 @@
+package dev.steady.application.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "survey_results")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SurveyResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Application application;
+
+    @Column(nullable = false)
+    private String question;
+
+    @Column(nullable = false)
+    private String answer;
+
+    @Column(nullable = false)
+    private int sequence;
+
+    private SurveyResult(Application application,
+                         String question,
+                         String answer,
+                         int sequence) {
+        application.addSurveyResult(this);
+        this.application = application;
+        this.question = question;
+        this.answer = answer;
+        this.sequence = sequence;
+    }
+
+    public static SurveyResult create(Application application, String question, String answer, int sequence) {
+        return new SurveyResult(application,
+                question,
+                answer,
+                sequence);
+    }
+
+    public void setApplication(Application application) {
+        this.application = application;
+    }
+
+}
