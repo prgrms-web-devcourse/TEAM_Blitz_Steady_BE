6513a37f7e5632ffb17659b135640e491a6f7428
weonest <geonhee33@naver.com>
2023-11-01 13:54:02 +0900

    :white_check_mark: SteadyServiceTest 리더가 아닌 요청에 대한 테스트

--- src/test/java/dev/steady/steady/service/SteadyServiceTest.java
+++ src/test/java/dev/steady/steady/service/SteadyServiceTest.java
@@ -1,8 +1,8 @@
 package dev.steady.steady.service;
 
-import dev.steady.global.auth.AuthFixture;
 import dev.steady.steady.domain.Steady;
 import dev.steady.steady.domain.SteadyStack;
+import dev.steady.steady.domain.SteadyStatus;
 import dev.steady.steady.domain.repository.ParticipantRepository;
 import dev.steady.steady.domain.repository.SteadyPositionRepository;
 import dev.steady.steady.domain.repository.SteadyQuestionRepository;
@@ -11,26 +11,28 @@ import dev.steady.steady.domain.repository.SteadyStackRepository;
 import dev.steady.steady.dto.request.SteadyPageRequest;
 import dev.steady.steady.dto.response.LeaderResponse;
 import dev.steady.steady.dto.response.SteadyStackResponse;
-import dev.steady.steady.fixture.SteadyFixtures;
 import dev.steady.user.domain.repository.PositionRepository;
 import dev.steady.user.domain.repository.StackRepository;
-import dev.steady.user.fixture.UserFixtures;
 import dev.steady.user.domain.repository.UserRepository;
-import org.hibernate.Hibernate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-import static dev.steady.global.auth.AuthFixture.*;
-import static dev.steady.steady.fixture.SteadyFixtures.*;
-import static dev.steady.user.fixture.UserFixtures.*;
+import static dev.steady.global.auth.AuthFixture.createUserInfo;
+import static dev.steady.steady.fixture.SteadyFixtures.createSteadyRequest;
+import static dev.steady.steady.fixture.SteadyFixtures.createSteadyUpdateRequest;
+import static dev.steady.user.fixture.UserFixtures.createAnotherPosition;
+import static dev.steady.user.fixture.UserFixtures.createAnotherStack;
+import static dev.steady.user.fixture.UserFixtures.createAnotherUser;
+import static dev.steady.user.fixture.UserFixtures.createPosition;
+import static dev.steady.user.fixture.UserFixtures.createStack;
+import static dev.steady.user.fixture.UserFixtures.createUser;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @Transactional
@@ -199,6 +201,55 @@ class SteadyServiceTest {
                 () -> assertThat(updatedSteady.getSteadyStacks()).hasSameSizeAs(steadyStacks)
                         .extracting("id").containsExactly(steadyStacks.get(0).getId())
         );
+    }
+
+    @Test
+    @DisplayName("리더가 아닌 유저가 수정 요청을 보내면 에러를 반환한다.")
+    void updateSteadyByAnotherUserTest() {
+        var position = createPosition();
+        var savedPosition = positionRepository.save(position);
+        var user = createUser(savedPosition);
+        var savedUser = userRepository.save(user);
+        var stack = createStack();
+        var savedStack = stackRepository.save(stack);
+        var userInfo = createUserInfo(savedUser.getId());
+
+        var steadyRequest = createSteadyRequest(savedStack.getId(), savedPosition.getId());
+        var steadyId = steadyService.create(steadyRequest, userInfo);
+
+        var anotherPosition = createAnotherPosition();
+        var savedAnotherPosition = positionRepository.save(anotherPosition);
+        var anotherStack = createAnotherStack();
+        var savedAnotherStack = stackRepository.save(anotherStack);
+
+        var anotherUser = createAnotherUser(savedAnotherPosition);
+        var anotherUserInfo = createUserInfo(anotherUser.getId());
+
+        var steadyUpdateRequest = createSteadyUpdateRequest(savedAnotherStack.getId(), savedAnotherPosition.getId());
+
+        assertThatThrownBy(() -> steadyService.updateSteady(steadyId, anotherUserInfo, steadyUpdateRequest))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("리더가 아닌 유저가 끌어올리기 요청을 보내면 에러를 반환한다.")
+    void promoteSteadyByAnotherUserTest() {
+        var position = createPosition();
+        var savedPosition = positionRepository.save(position);
+        var user = createUser(savedPosition);
+        var savedUser = userRepository.save(user);
+        var stack = createStack();
+        var savedStack = stackRepository.save(stack);
+        var userInfo = createUserInfo(savedUser.getId());
+
+        var steadyRequest = createSteadyRequest(savedStack.getId(), savedPosition.getId());
+        var steadyId = steadyService.create(steadyRequest, userInfo);
+
+        var anotherUser = createAnotherUser(savedPosition);
+        var anotherUserInfo = createUserInfo(anotherUser.getId());
+
+        assertThatThrownBy(() -> steadyService.promoteSteady(steadyId, anotherUserInfo))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
