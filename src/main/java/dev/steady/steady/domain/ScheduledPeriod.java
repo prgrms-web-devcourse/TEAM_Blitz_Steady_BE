b11a3e1b94f1fdc74803de54d8a538e5409dd5dc
weonest <geonhee33@naver.com>
2023-11-01 15:37:01 +0900

    :sparkles: openingDate -> ScheduledPeriod 생성

---
+++ src/main/java/dev/steady/steady/domain/ScheduledPeriod.java
@@ -0,0 +1,19 @@
+package dev.steady.steady.domain;
+
+public enum ScheduledPeriod {
+
+    TO_BE_DETERMINED,
+    ONE_WEEK,
+    TWO_WEEK,
+    THREE_WEEK,
+    FOUR_WEEK,
+    FIVE_WEEK,
+    ONE_MONTH,
+    TWO_MONTH,
+    THREE_MONTH,
+    FOUR_MONTH,
+    FIVE_MONTH,
+    SIX_MONTH,
+    LONG_TERM
+
+}
