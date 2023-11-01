5a340cc0f2423442bd2411be6df2e43b88dcbb8e
na-yk <ninghao17@naver.com>
2023-11-01 11:33:28 +0900

    :sparkles: User 생성 기능 service 추가

---
+++ src/main/java/dev/steady/user/service/UserService.java
@@ -0,0 +1,56 @@
+package dev.steady.user.service;
+
+import dev.steady.user.domain.Position;
+import dev.steady.user.domain.Stack;
+import dev.steady.user.domain.User;
+import dev.steady.user.domain.UserStack;
+import dev.steady.user.domain.repository.PositionRepository;
+import dev.steady.user.domain.repository.StackRepository;
+import dev.steady.user.domain.repository.UserRepository;
+import dev.steady.user.domain.repository.UserStackRepository;
+import dev.steady.user.dto.request.CreateUserRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final StackRepository stackRepository;
+    private final PositionRepository positionRepository;
+    private final UserStackRepository userStackRepository;
+
+    @Transactional
+    public Long createUser(CreateUserRequest request) {
+        Position position = getPosition(request.positionId());
+
+        User user = request.toEntity(position);
+        User savedUser = userRepository.save(user);
+
+        List<UserStack> userStacks = createUserStacks(request.stackIds(), savedUser);
+        userStackRepository.saveAll(userStacks);
+
+        return savedUser.getId();
+    }
+
+    private List<Stack> getStacks(List<Long> stackIds) {
+        return stackIds.stream()
+                .map(stackId -> stackRepository.findById(stackId).orElseThrow(IllegalArgumentException::new))
+                .toList();
+    }
+
+    private Position getPosition(Long positionId) {
+        return positionRepository.findById(positionId).orElseThrow(IllegalArgumentException::new);
+    }
+
+    private List<UserStack> createUserStacks(List<Long> stackIds, User user) {
+        List<Stack> stacks = getStacks(stackIds);
+        return stacks.stream().map(stack -> new UserStack(user, stack)).toList();
+    }
+
+}
