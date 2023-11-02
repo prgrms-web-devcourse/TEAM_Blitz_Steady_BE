b5c23e8e794ec0c726a886e70f385ccf0af58944
na-yk <ninghao17@naver.com>
2023-11-02 16:25:14 +0900

    :white_check_mark: UserServiceTest @Transactional 사용하지 않도록 개선

--- src/test/java/dev/steady/user/service/UserServiceTest.java
+++ src/test/java/dev/steady/user/service/UserServiceTest.java
@@ -1,47 +1,64 @@
 package dev.steady.user.service;
 
 import dev.steady.user.domain.Stack;
+import dev.steady.user.domain.User;
 import dev.steady.user.domain.repository.PositionRepository;
 import dev.steady.user.domain.repository.StackRepository;
 import dev.steady.user.domain.repository.UserRepository;
 import dev.steady.user.domain.repository.UserStackRepository;
 import dev.steady.user.dto.request.UserCreateRequest;
-import dev.steady.user.fixture.UserFixtures;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.stream.IntStream;
 
+import static dev.steady.user.fixture.UserFixtures.createPosition;
+import static dev.steady.user.fixture.UserFixtures.createStack;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-@Transactional
 @SpringBootTest
 class UserServiceTest {
 
     @Autowired
     private UserService userService;
+
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private StackRepository stackRepository;
+
     @Autowired
     private PositionRepository positionRepository;
+
     @Autowired
     private UserStackRepository userStackRepository;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
+    @AfterEach
+    void tearDown() {
+        userStackRepository.deleteAll();
+        userRepository.deleteAll();
+        positionRepository.deleteAll();
+        stackRepository.deleteAll();
+    }
 
     @DisplayName("프로필 정보를 입력 받아 유저를 생성한다.")
     @Test
     void createUser() {
         // given
-        var savedPosition = positionRepository.save(UserFixtures.createPosition());
-        var savedStacks = stackRepository.saveAll(
-                IntStream.range(0, 3)
-                        .mapToObj(i -> UserFixtures.createStack())
-                        .toList());
+        var savedPosition = positionRepository.save(createPosition());
+        var stacks = IntStream.range(0, 3)
+                .mapToObj(i -> createStack())
+                .toList();
+        var savedStacks = stackRepository.saveAll(stacks);
         var stackIds = savedStacks.stream().map(Stack::getId).toList();
 
         // when
@@ -50,8 +67,13 @@ class UserServiceTest {
 
         var userStacks = userStackRepository.findAllByUserId(userId);
 
-        // then
-        var user = userRepository.findById(userId).get();
+
+        User user = transactionTemplate.execute(status -> {
+            var foundUser = userRepository.findById(userId).get();
+            foundUser.getPosition().getName();
+            return foundUser;
+        });
+
         assertAll(
                 () -> assertThat(user.getId()).isEqualTo(userId),
                 () -> assertThat(user.getPosition()).isEqualTo(savedPosition),
