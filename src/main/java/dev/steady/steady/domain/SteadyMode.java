96b6507bef40be64b3ff4a117071984800db558e
weonest <geonhee33@naver.com>
2023-11-03 17:23:34 +0900

    :sparkles: SteadyMode from 메서드 추가

--- src/main/java/dev/steady/steady/domain/SteadyMode.java
+++ src/main/java/dev/steady/steady/domain/SteadyMode.java
@@ -1,9 +1,21 @@
 package dev.steady.steady.domain;
 
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+
+@NoArgsConstructor
 public enum SteadyMode {
 
     ONLINE,
     OFFLINE,
-    BOTH
+    BOTH;
+
+    public static SteadyMode from(String mode) {
+        return Arrays.stream(SteadyMode.values())
+                .filter(v -> v.name().equals(mode.toUpperCase()))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 
 }
