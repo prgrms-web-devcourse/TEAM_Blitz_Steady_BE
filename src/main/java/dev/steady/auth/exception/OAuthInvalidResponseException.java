f6897cba85a5ba66939ce5bf730c9229fdde6680
na-yk <ninghao17@naver.com>
2023-11-04 23:09:00 +0900

    :art: 컨벤션 코드 포맷팅

--- src/main/java/dev/steady/auth/exception/OAuthInvalidResponseException.java
+++ src/main/java/dev/steady/auth/exception/OAuthInvalidResponseException.java
@@ -2,10 +2,8 @@ package dev.steady.auth.exception;
 
 public class OAuthInvalidResponseException extends RuntimeException {
 
-    private final String message;
-
     public OAuthInvalidResponseException(String message) {
-        this.message = message;
+        super(message);
     }
 
 }
