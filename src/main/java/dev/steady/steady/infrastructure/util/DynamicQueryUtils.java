5e08d48ab1e3a2b42802db182683e60abb0e1d3a
weonest <geonhee33@naver.com>
2023-11-04 17:02:10 +0900

    :sparkles: 다이나믹 쿼리 유틸 클래스 추가

---
+++ src/main/java/dev/steady/steady/infrastructure/util/DynamicQueryUtils.java
@@ -0,0 +1,33 @@
+package dev.steady.steady.infrastructure.util;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class DynamicQueryUtils {
+
+    public static <T> BooleanExpression filterCondition(T condition, Function<T, BooleanExpression> function) {
+        T conditionResult = condition;
+
+        if (condition instanceof String str && !StringUtils.hasText(str)) {
+            conditionResult = null;
+        }
+
+        if (condition instanceof List list && CollectionUtils.isEmpty(list)) {
+            conditionResult = null;
+        }
+
+        return Optional.ofNullable(conditionResult)
+                .map(function)
+                .orElse(null);
+    }
+
+    public static <T> BooleanExpression equalCompare(T value, Function<T, BooleanExpression> function) {
+        return value == null ? null : function.apply(value);
+    }
+
+}
