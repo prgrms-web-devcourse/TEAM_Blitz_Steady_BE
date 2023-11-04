614e8de9af3230d34417d0e7fab6658d580ab99b
weonest <geonhee33@naver.com>
2023-11-04 17:01:30 +0900

    :art: 팩토리 메서드 추가

--- src/main/java/dev/steady/steady/domain/SteadyStatus.java
+++ src/main/java/dev/steady/steady/domain/SteadyStatus.java
@@ -1,9 +1,18 @@
 package dev.steady.steady.domain;
 
+import java.util.Arrays;
+
 public enum SteadyStatus {
 
     RECRUITING,
     CLOSED,
-    FINISHED
+    FINISHED;
+
+    public static SteadyStatus from(String status) {
+        return Arrays.stream(SteadyStatus.values())
+                .filter(v -> v.name().equals(status.toUpperCase()))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 
 }
