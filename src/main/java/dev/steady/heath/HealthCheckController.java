a7bbc4bc28f1835d8b6d236b7c0559253b071497
jun <alap_u@naver.com>
2023-11-05 20:37:37 +0900

    :sparkles: 헬스체크 GetMapping 추가

--- src/main/java/dev/steady/heath/HealthCheckController.java
+++ src/main/java/dev/steady/heath/HealthCheckController.java
@@ -1,6 +1,7 @@
 package dev.steady.heath;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthCheckController {
 
+    @GetMapping
     public ResponseEntity<Void> healthCheck() {
         return ResponseEntity.ok().build();
     }
