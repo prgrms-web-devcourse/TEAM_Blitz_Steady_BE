571c3a9f6dbaf7f68bf53f146ca9495f2907ae27
na-yk <ninghao17@naver.com>
2023-11-05 22:35:03 +0900

    :white_check_mark: 최초 로그인 사용자 Controller 테스트 추가

--- src/test/java/dev/steady/auth/presentation/OAuthControllerTest.java
+++ src/test/java/dev/steady/auth/presentation/OAuthControllerTest.java
@@ -1,8 +1,10 @@
 package dev.steady.auth.presentation;
 
 import dev.steady.auth.domain.Platform;
+import dev.steady.auth.oauth.dto.response.LogInResponse;
 import dev.steady.global.config.ControllerTestConfig;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
@@ -10,6 +12,10 @@ import java.net.URI;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.resourceDetails;
+import static dev.steady.auth.fixture.OAuthFixture.createAuthCode;
+import static dev.steady.auth.fixture.OAuthFixture.createLogInResponseForUserNotExist;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
@@ -17,10 +23,17 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.JsonFieldType.BOOLEAN;
+import static org.springframework.restdocs.payload.JsonFieldType.NULL;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -52,6 +65,41 @@ class OAuthControllerTest extends ControllerTestConfig {
                         )))
                 .andExpect(header().string("Location", authCodeRequestUrl.toString()));
 
+    }
+
+    @Test
+    @DisplayName("카카오 로그인을 하여 계정 ID와 최초 로그인 여부를 반환할 수 있다.")
+    void kakaoLogInforUserNotExist() throws Exception {
+        // given
+        String platform = Platform.KAKAO.name().toLowerCase();
+        String authCode = createAuthCode();
+        LogInResponse logInResponse = createLogInResponseForUserNotExist();
+        given(oAuthService.logIn(any(), any())).willReturn(logInResponse);
+
+        // when, then
+        mockMvc.perform(get("/api/v1/auth/{platform}/callback", platform)
+                        .queryParam("code", authCode))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("auth-v1-login-new", resourceDetails().tag("인증")
+                                .description("카카오 로그인-최초"),
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("platform").description("소셜로그인 플랫폼")
+                        ),
+                        queryParameters(
+                                parameterWithName("code").description("카카오 인가 코드")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type(NUMBER).description("계정 id"),
+                                fieldWithPath("isNew").type(BOOLEAN).description("최초 로그인 여부"),
+                                fieldWithPath("token").type(NULL).description("서비스 이용 토큰")
+                        )))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(logInResponse.id()))
+                .andExpect(jsonPath("isNew").isBoolean())
+                .andExpect(jsonPath("token").isEmpty());
     }
 
 }
