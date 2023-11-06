905e293f0ecdd778747fa72125fd10ec805efde9
jun <alap_u@naver.com>
2023-11-06 23:55:44 +0900

    :white_check_mark: 신청서 컨트롤러 테스트 코드 및 레스트독 문서화 작성

--- src/test/java/dev/steady/application/fixture/ApplicationFixture.java
+++ src/test/java/dev/steady/application/fixture/ApplicationFixture.java
@@ -2,6 +2,10 @@ package dev.steady.application.fixture;
 
 import dev.steady.application.domain.Application;
 import dev.steady.application.domain.SurveyResult;
+import dev.steady.application.dto.response.ApplicationDetailResponse;
+import dev.steady.application.dto.response.ApplicationSummaryResponse;
+import dev.steady.application.dto.response.SliceResponse;
+import dev.steady.application.dto.response.SurveyResultResponse;
 import dev.steady.steady.domain.Steady;
 import dev.steady.user.domain.User;
 
@@ -13,6 +17,25 @@ public class ApplicationFixture {
         Application application = new Application(user, steady);
         saveSurveyResults(application);
         return application;
+    }
+
+    public static SliceResponse<ApplicationSummaryResponse> createApplicationSummaryResponse() {
+        return new SliceResponse<>(
+                List.of(new ApplicationSummaryResponse(1L, "닉네임", "profile.url"),
+                        new ApplicationSummaryResponse(2L, "닉네임2", "profile.url"),
+                        new ApplicationSummaryResponse(3L, "닉네임3", "profile.url")),
+                3,
+                false);
+    }
+
+    public static ApplicationDetailResponse createApplicationDetailResponse() {
+        return new ApplicationDetailResponse(
+                List.of(
+                        new SurveyResultResponse("질문", "답변"),
+                        new SurveyResultResponse("질문1", "답변1"),
+                        new SurveyResultResponse("질문3", "답변2")
+                )
+        );
     }
 
     private static void saveSurveyResults(Application application) {
