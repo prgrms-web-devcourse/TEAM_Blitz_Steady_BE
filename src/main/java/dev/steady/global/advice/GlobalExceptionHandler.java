a126b6bdab548e8b0c3089fca57e88b868c750db
jun <alap_u@naver.com>
2023-11-07 16:14:47 +0900

    :sparkles: ExceptionHandler 정의

---
+++ src/main/java/dev/steady/global/advice/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package dev.steady.global.advice;
+
+import dev.steady.global.response.ErrorResponse;
+import dev.steady.global.exception.GlobalErrorCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(Exception e) {
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.of(GlobalErrorCode.INTERNAL_SERVER_ERROR));
+    }
+
+}
