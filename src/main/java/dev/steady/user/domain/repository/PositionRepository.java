7b04ba611cf62845d2f696456eab434dea231614
weonest <geonhee33@naver.com>
2023-11-09 17:09:16 +0900

    :sparkles: Stack & Position 예외 코드 추가 및 findById -> getById

--- src/main/java/dev/steady/user/domain/repository/PositionRepository.java
+++ src/main/java/dev/steady/user/domain/repository/PositionRepository.java
@@ -1,8 +1,16 @@
 package dev.steady.user.domain.repository;
 
+import dev.steady.global.exception.NotFoundException;
 import dev.steady.user.domain.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import static dev.steady.user.exception.PositionErrorCode.POSITION_NOT_FOUND;
+
 public interface PositionRepository extends JpaRepository<Position, Long> {
+
+    default Position getById(Long positionId) {
+        return findById(positionId)
+                .orElseThrow(() -> new NotFoundException(POSITION_NOT_FOUND));
+    }
 
 }
--- src/main/java/dev/steady/user/domain/repository/StackRepository.java
+++ src/main/java/dev/steady/user/domain/repository/StackRepository.java
@@ -1,8 +1,17 @@
 package dev.steady.user.domain.repository;
 
+import dev.steady.global.exception.NotFoundException;
 import dev.steady.user.domain.Stack;
+import dev.steady.user.exception.StackErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import static dev.steady.user.exception.StackErrorCode.*;
+
 public interface StackRepository extends JpaRepository<Stack, Long> {
+
+    default Stack getById(Long stackId) {
+        return findById(stackId)
+                .orElseThrow(() -> new NotFoundException(STACK_NOT_FOUND));
+    }
 
 }
