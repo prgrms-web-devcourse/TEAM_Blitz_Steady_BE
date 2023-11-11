ca929bfd01a2a7b9ca80cd5cec8f5bee1386c7a4
weonest <geonhee33@naver.com>
2023-11-11 19:05:20 +0900

    :bug: ErrorCode null 반환 수정

--- src/main/java/dev/steady/user/exception/PositionErrorCode.java
+++ src/main/java/dev/steady/user/exception/PositionErrorCode.java
@@ -16,12 +16,12 @@ public enum PositionErrorCode implements ErrorCode {
 
     @Override
     public String code() {
-        return null;
+        return code;
     }
 
     @Override
     public String message() {
-        return null;
+        return message;
     }
 
 }
