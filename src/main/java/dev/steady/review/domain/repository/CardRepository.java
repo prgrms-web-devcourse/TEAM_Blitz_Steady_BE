18432a7122e540f87d4e759104f26b261cb2053b
na-yk <ninghao17@naver.com>
2023-11-13 04:05:19 +0900

    :sparkles: CardRepository 추가

---
+++ src/main/java/dev/steady/review/domain/repository/CardRepository.java
@@ -0,0 +1,16 @@
+package dev.steady.review.domain.repository;
+
+import dev.steady.global.exception.NotFoundException;
+import dev.steady.review.domain.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import static dev.steady.review.exception.ReviewErrorCode.CARD_NOT_FOUND;
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+    default Card getById(Long cardId) {
+        return findById(cardId)
+                .orElseThrow(() -> new NotFoundException(CARD_NOT_FOUND));
+    }
+    
+}
