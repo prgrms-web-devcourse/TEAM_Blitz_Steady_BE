4b6c7f9ae3d5f085ac465ccdc9f7bbbada991020
na-yk <ninghao17@naver.com>
2023-11-12 20:16:20 +0900

    :recycle: UserRepository default 메서드 NotFoudException 적용

--- src/main/java/dev/steady/user/domain/repository/UserRepository.java
+++ src/main/java/dev/steady/user/domain/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package dev.steady.user.domain.repository;
 
+import dev.steady.global.exception.NotFoundException;
 import dev.steady.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import static dev.steady.user.exception.UserErrorCode.USER_NOT_FOUND;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     default User getUserBy(Long userId) {
         return findById(userId)
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
     }
 
     boolean existsByNickname(String nickname);
