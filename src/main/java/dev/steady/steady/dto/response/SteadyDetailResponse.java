dbedb82158dfd377b66c4437ca4e674a3440d4f4
weonest <geonhee33@naver.com>
2023-11-13 18:20:15 +0900

    :sparkles: 상세 조회 응답 객체 생성일 추가

--- src/main/java/dev/steady/steady/dto/response/SteadyDetailResponse.java
+++ src/main/java/dev/steady/steady/dto/response/SteadyDetailResponse.java
@@ -8,6 +8,7 @@ import dev.steady.steady.domain.SteadyStatus;
 import dev.steady.steady.domain.SteadyType;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record SteadyDetailResponse(
@@ -28,7 +29,8 @@ public record SteadyDetailResponse(
         List<SteadyStackResponse> stacks,
         boolean isLeader,
         boolean isSubmittedUser,
-        int promotionCount
+        int promotionCount,
+        LocalDateTime createdAt
 ) {
 
     public static SteadyDetailResponse of(Steady steady, List<SteadyPosition> positions, boolean isLeader, boolean isSubmittedUser) {
@@ -54,7 +56,8 @@ public record SteadyDetailResponse(
                         .toList(),
                 isLeader,
                 isSubmittedUser,
-                steady.getPromotionCount()
+                steady.getPromotionCount(),
+                steady.getCreatedAt()
         );
     }
 
