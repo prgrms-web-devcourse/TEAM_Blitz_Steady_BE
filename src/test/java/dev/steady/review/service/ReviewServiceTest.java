a8e2a1524b9b3e5907c6341ef6574391cf398472
na-yk <ninghao17@naver.com>
2023-11-14 00:44:39 +0900

    :art: ReviewFixtures->ReviewFixture 클래스명 변경

--- src/test/java/dev/steady/review/service/ReviewServiceTest.java
+++ src/test/java/dev/steady/review/service/ReviewServiceTest.java
@@ -26,8 +26,8 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 import static dev.steady.global.auth.AuthFixture.createUserInfo;
-import static dev.steady.review.fixture.ReviewFixtures.createCard;
-import static dev.steady.review.fixture.ReviewFixtures.createReviewCreateRequest;
+import static dev.steady.review.fixture.ReviewFixture.createCard;
+import static dev.steady.review.fixture.ReviewFixture.createReviewCreateRequest;
 import static dev.steady.steady.fixture.SteadyFixtures.createFinishedSteady;
 import static dev.steady.steady.fixture.SteadyFixtures.createParticipant;
 import static dev.steady.user.fixture.UserFixtures.createPosition;
