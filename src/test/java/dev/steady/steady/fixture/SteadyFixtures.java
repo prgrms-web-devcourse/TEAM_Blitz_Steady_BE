f121a211d5d5f7dac830c4dd70651e6254c50918
weonest <geonhee33@naver.com>
2023-11-14 16:10:03 +0900

    :white_check_mark: 스테디 질문 조회 로직 테스트

--- src/test/java/dev/steady/steady/fixture/SteadyFixtures.java
+++ src/test/java/dev/steady/steady/fixture/SteadyFixtures.java
@@ -1,6 +1,5 @@
 package dev.steady.steady.fixture;
 
-import dev.steady.steady.domain.Participant;
 import dev.steady.steady.domain.ScheduledPeriod;
 import dev.steady.steady.domain.Steady;
 import dev.steady.steady.domain.SteadyMode;
@@ -10,10 +9,7 @@ import dev.steady.steady.domain.SteadyStatus;
 import dev.steady.steady.domain.SteadyType;
 import dev.steady.steady.dto.request.SteadyCreateRequest;
 import dev.steady.steady.dto.request.SteadyUpdateRequest;
-import dev.steady.steady.dto.response.PageResponse;
-import dev.steady.steady.dto.response.ParticipantResponse;
-import dev.steady.steady.dto.response.ParticipantsResponse;
-import dev.steady.steady.dto.response.SteadySearchResponse;
+import dev.steady.steady.dto.response.*;
 import dev.steady.user.domain.Position;
 import dev.steady.user.domain.Stack;
 import dev.steady.user.domain.User;
@@ -158,8 +154,11 @@ public class SteadyFixtures {
         ));
     }
 
-    public static Participant createParticipant(User user, Steady steady) {
-        return new Participant(user, steady, false);
+    public static SteadyQuestionsResponse createSteadyQuestionsResponse() {
+        return new SteadyQuestionsResponse(List.of(
+                new SteadyQuestionResponse(1L, "누구세요?", 1),
+                new SteadyQuestionResponse(2L, "뭐세요?", 2)
+        ));
     }
 
 }
