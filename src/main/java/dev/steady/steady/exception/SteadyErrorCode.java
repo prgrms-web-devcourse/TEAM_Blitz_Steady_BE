e13fa6347bc48cd20cc087779f7def2785dbc4eb
weonest <geonhee33@naver.com>
2023-11-14 16:48:26 +0900

    :sparkles: 스테디 종료일 및 종료 검증 로직 추가

--- src/main/java/dev/steady/steady/exception/SteadyErrorCode.java
+++ src/main/java/dev/steady/steady/exception/SteadyErrorCode.java
@@ -4,11 +4,12 @@ import dev.steady.global.exception.ErrorCode;
 
 public enum SteadyErrorCode implements ErrorCode {
 
-    STEADY_NOT_FOUND("S100", "해당 Steady를 찾을 수 없습니다."),
-    STEADY_IS_NOT_EMPTY("S101", "Steady에 리더를 제외한 참여자가 존재합니다."),
-    LEADER_PERMISSION_NEEDED("S102", "Steady 리더 권한이 필요합니다."),
+    STEADY_NOT_FOUND("S100", "해당 스테디를 찾을 수 없습니다."),
+    STEADY_IS_NOT_EMPTY("S101", "스테디에 리더를 제외한 참여자가 존재합니다."),
+    LEADER_PERMISSION_NEEDED("S102", "스테디 리더 권한이 필요합니다."),
     PROMOTION_COUNT_EXCEPTION("S103", "제공된 끌어올리기를 모두 사용하였습니다."),
-    PARTICIPANT_LIMIT_EXCEEDED("S104", "Steady 정원을 초과할 수 없습니다.");
+    PARTICIPANT_LIMIT_EXCEEDED("S104", "스테디 정원을 초과할 수 없습니다."),
+    ALREADY_FINISHED("S105", "이미 종료된 스테디입니다.");
 
     private final String code;
     private final String message;
