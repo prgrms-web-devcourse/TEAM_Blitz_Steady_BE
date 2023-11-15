88f0b96d2db46fa69123b0fa8c3ab5dca6162ff3
na-yk <ninghao17@naver.com>
2023-11-15 16:53:02 +0900

    :sparkles: User가 받은 Review 중 공개된 Comment만 조회하는 Repository 코드 추가

---
+++ src/main/java/dev/steady/review/infrastructure/ReviewQueryRepositoryImpl.java
@@ -0,0 +1,39 @@
+package dev.steady.review.infrastructure;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import dev.steady.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static dev.steady.review.domain.QReview.review;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ReviewQueryRepositoryImpl implements ReviewQueryRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<String> findPublicCommentsByRevieweeUser(User user) {
+        return jpaQueryFactory.select(review.comment)
+                .from(review)
+                .where(revieweeEqualsUser(user),
+                        isPublic())
+                .orderBy(review.createdAt.desc())
+                .fetch();
+    }
+
+    private BooleanExpression revieweeEqualsUser(User user) {
+        return review.reviewee.user.eq(user);
+    }
+
+    private BooleanExpression isPublic() {
+        return review.isPublic.isTrue();
+    }
+
+}
