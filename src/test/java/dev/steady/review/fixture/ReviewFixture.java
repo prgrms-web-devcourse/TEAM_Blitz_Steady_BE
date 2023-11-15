21672f0dd8b4d959d9adcba3b9e066b2167c6a77
na-yk <ninghao17@naver.com>
2023-11-15 16:57:19 +0900

    :white_check_mark: 타인의 프로필 조회 기능 Service 테스트 추가

--- src/test/java/dev/steady/review/fixture/ReviewFixture.java
+++ src/test/java/dev/steady/review/fixture/ReviewFixture.java
@@ -1,7 +1,12 @@
 package dev.steady.review.fixture;
 
 import dev.steady.review.domain.Card;
+import dev.steady.review.domain.Review;
+import dev.steady.review.domain.UserCard;
 import dev.steady.review.dto.ReviewCreateRequest;
+import dev.steady.steady.domain.Participant;
+import dev.steady.steady.domain.Steady;
+import dev.steady.user.domain.User;
 
 import java.util.List;
 
@@ -19,6 +24,19 @@ public class ReviewFixture {
         return new Card(
                 "이 팀원은 출석을 열심히 잘 했어요!"
         );
+    }
+
+    public static Review createReview(Participant reviewer, Participant reviewee, Steady steady) {
+        return Review.builder()
+                .steady(steady)
+                .reviewer(reviewer)
+                .reviewee(reviewee)
+                .comment("다음에도 저랑 같이 공부해요!")
+                .build();
+    }
+
+    public static UserCard createUserCard(User user, Card card) {
+        return new UserCard(user, card);
     }
 
 }
