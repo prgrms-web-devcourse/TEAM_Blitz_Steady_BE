0aad8b7ff9ea805b0793e013a1f31fcc80ee1a0d
na-yk <ninghao17@naver.com>
2023-11-15 19:56:39 +0900

    :sparkles: ReviewRepository getById default 메서드 추가

--- src/main/java/dev/steady/review/domain/repository/ReviewRepository.java
+++ src/main/java/dev/steady/review/domain/repository/ReviewRepository.java
@@ -1,11 +1,18 @@
 package dev.steady.review.domain.repository;
 
+import dev.steady.global.exception.NotFoundException;
 import dev.steady.review.domain.Review;
 import dev.steady.steady.domain.Participant;
 import dev.steady.steady.domain.Steady;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import static dev.steady.review.exception.ReviewErrorCode.REVIEW_NOT_FOUND;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    default Review getById(Long reviewId) {
+        return findById(reviewId)
+                .orElseThrow(() -> new NotFoundException(REVIEW_NOT_FOUND));
+    }
 
     boolean existsByReviewerAndRevieweeAndSteady(Participant reviewer, Participant reviewee, Steady steady);
 
