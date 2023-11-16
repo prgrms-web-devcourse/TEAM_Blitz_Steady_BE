c7c2349b048538000f11b1a09f4976878ed5d526
weonest <geonhee33@naver.com>
2023-11-16 17:26:11 +0900

    :fire: 구조 변경으로 인한 삭제

--- src/main/java/dev/steady/notification/NotificationRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.steady.notification;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
