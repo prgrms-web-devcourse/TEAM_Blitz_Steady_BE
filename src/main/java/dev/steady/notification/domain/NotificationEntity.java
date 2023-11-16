2d1617fe528ec5034482006efb3c9720a00ced7b
weonest <geonhee33@naver.com>
2023-11-16 17:27:32 +0900

    :sparkles: 알림 엔티티, 레포지, 에러코드 추가

--- src/main/java/dev/steady/notification/domain/NotificationEntity.java
+++ src/main/java/dev/steady/notification/domain/NotificationEntity.java
@@ -1,5 +1,6 @@
-package dev.steady.notification;
+package dev.steady.notification.domain;
 
+import dev.steady.global.entity.BaseEntity;
 import dev.steady.user.domain.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,22 +8,19 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import static dev.steady.notification.NotificationType.APPLICATION;
-import static dev.steady.notification.NotificationType.CHAT;
-import static dev.steady.notification.NotificationType.COMMENT;
-import static dev.steady.notification.NotificationType.RECRUITMENT;
 
 @Entity
 @Getter
 @Table(name = "notifications")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Notification {
+public class NotificationEntity extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,20 +39,20 @@ public class Notification {
     private boolean isRead;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @Column(name = "user_id")
+    @JoinColumn(name = "user_id")
     private User receiver;
 
-    private Notification(User receiver, NotificationType type) {
-        this.receiver = receiver;
+    @Builder
+    private NotificationEntity(NotificationType type, String content, String redirectUri, User receiver) {
         this.type = type;
+        this.content = content;
+        this.redirectUri = redirectUri;
+        this.isRead = false;
+        this.receiver = receiver;
     }
 
-    public static Notification createRecruitNotification(User receiver) {
-        return new Notification(receiver, RECRUITMENT);
-    }
-
-    public static Notification createApplicationNotification(User receiver) {
-        return new Notification(receiver, APPLICATION);
+    public void read() {
+        this.isRead = true;
     }
 
 }
---
+++ src/main/java/dev/steady/notification/domain/repository/NotificationRepository.java
@@ -0,0 +1,22 @@
+package dev.steady.notification.domain.repository;
+
+import dev.steady.global.exception.NotFoundException;
+import dev.steady.notification.domain.NotificationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+import static dev.steady.notification.exception.NotificationErrorCode.*;
+
+public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
+
+    List<NotificationEntity> findByReceiverId(Long receiverId);
+
+    void deleteByReceiverId(Long receiverId);
+
+    default NotificationEntity getById(Long notificationId) {
+        return findById(notificationId)
+                .orElseThrow(() -> new NotFoundException(NOTIFICATION_NOT_FOUND));
+    }
+
+}
