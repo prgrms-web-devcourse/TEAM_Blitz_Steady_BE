6721e6e89d6d2e5a70c456ecac5400699f839e39
weonest <geonhee33@naver.com>
2023-11-20 23:14:16 +0900

    :recycle: 알림 Redirect URI 변경

--- src/main/java/dev/steady/notification/domain/FreshApplicationNotificationStrategy.java
+++ src/main/java/dev/steady/notification/domain/FreshApplicationNotificationStrategy.java
@@ -18,7 +18,7 @@ public class FreshApplicationNotificationStrategy extends NotificationStrategy {
 
     @Override
     public String getRedirectUri() {
-        return String.format("/steady/detail/%d", steady.getId());
+        return String.format("/mypage/application");
     }
 
 }
