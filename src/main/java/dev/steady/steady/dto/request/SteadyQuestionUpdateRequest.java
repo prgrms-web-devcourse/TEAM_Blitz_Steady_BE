c7e554ea17a96d8c7e88f7247d8338ee86d72afe
weonest <geonhee33@naver.com>
2023-11-20 18:32:10 +0900

    :sparkles: Bean Validation 추가

--- src/main/java/dev/steady/steady/dto/request/SteadyQuestionUpdateRequest.java
+++ src/main/java/dev/steady/steady/dto/request/SteadyQuestionUpdateRequest.java
@@ -1,8 +1,11 @@
 package dev.steady.steady.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public record SteadyQuestionUpdateRequest(
+        @NotNull(message = "NULL은 올 수 없습니다." )
         List<String> questions
 ) {
 }
