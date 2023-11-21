31f43b4519a76ca1756926228bea757f648b7dc6
jun <alap_u@naver.com>
2023-11-22 05:38:59 +0900

    :white_check_mark: 본인의 신청서 목록 조회 테스트

--- src/main/java/dev/steady/application/dto/response/MyApplicationSummaryResponse.java
+++ src/main/java/dev/steady/application/dto/response/MyApplicationSummaryResponse.java
@@ -1,4 +1,4 @@
-package dev.steady.application.service;
+package dev.steady.application.dto.response;
 
 import dev.steady.application.domain.Application;
 import dev.steady.application.domain.ApplicationStatus;
--- src/test/java/dev/steady/application/fixture/ApplicationFixture.java
+++ src/test/java/dev/steady/application/fixture/ApplicationFixture.java
@@ -4,12 +4,18 @@ import dev.steady.application.domain.Application;
 import dev.steady.application.domain.SurveyResult;
 import dev.steady.application.dto.response.ApplicationDetailResponse;
 import dev.steady.application.dto.response.ApplicationSummaryResponse;
+import dev.steady.application.dto.response.MyApplicationSummaryResponse;
 import dev.steady.application.dto.response.SliceResponse;
 import dev.steady.application.dto.response.SurveyResultResponse;
 import dev.steady.steady.domain.Steady;
 import dev.steady.user.domain.User;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static dev.steady.application.domain.ApplicationStatus.ACCEPTED;
+import static dev.steady.application.domain.ApplicationStatus.REJECTED;
+import static dev.steady.application.domain.ApplicationStatus.WAITING;
 
 public class ApplicationFixture {
 
@@ -25,6 +31,17 @@ public class ApplicationFixture {
                         new ApplicationSummaryResponse(3L, 12L, "닉네임3", "profile.url")),
                 3,
                 false);
+    }
+
+    public static SliceResponse<MyApplicationSummaryResponse> createMyApplicationSummaryResponse() {
+        return new SliceResponse<>(
+                List.of(
+                        new MyApplicationSummaryResponse(1L, "스테디 제목1", LocalDateTime.of(2023, 12, 31, 12, 30), WAITING),
+                        new MyApplicationSummaryResponse(2L, "스테디 제목2", LocalDateTime.of(2023, 12, 31, 12, 30), REJECTED),
+                        new MyApplicationSummaryResponse(3L, "스테디 제목3", LocalDateTime.of(2023, 12, 31, 12, 30), ACCEPTED)),
+                3,
+                false
+        );
     }
 
     public static ApplicationDetailResponse createApplicationDetailResponse() {
