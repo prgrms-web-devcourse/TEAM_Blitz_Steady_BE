be3dcc9467bdaa353a8fb12adf9229c656de8aa6
na-yk <ninghao17@naver.com>
2023-11-23 18:21:42 +0900

    :sparkles: CardResponse imageUrl 필드 추가

--- src/main/java/dev/steady/review/dto/response/CardResponse.java
+++ src/main/java/dev/steady/review/dto/response/CardResponse.java
@@ -4,13 +4,15 @@ import dev.steady.review.domain.Card;
 
 public record CardResponse(
         Long cardId,
-        String content
+        String content,
+        String imageUrl
 ) {
 
     public static CardResponse from(Card card) {
         return new CardResponse(
                 card.getId(),
-                card.getContent()
+                card.getContent(),
+                card.getImageUrl()
         );
     }
 
