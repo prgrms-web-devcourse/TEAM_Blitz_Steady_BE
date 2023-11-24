59ce1486785d3cc925b945574edc9100c49fb9a8
na-yk <ninghao17@naver.com>
2023-11-24 11:46:11 +0900

    :sparkle: PresignedUrlProvider 추가
    AWS S3에 파일을 업로드할 수 있는 PresignedUrl을 생성함

---
+++ src/main/java/dev/steady/storage/PresignedUrlProvier.java
@@ -0,0 +1,62 @@
+package dev.steady.storage;
+
+import dev.steady.global.exception.InvalidValueException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+import java.util.UUID;
+
+import static dev.steady.storage.exception.StorageErrorCode.NOT_SUPPORTED_FILE_TYPE;
+
+@Component
+public class PresignedUrlProvier {
+
+    private static final String CONTENT_TYPE_PATTERN = "image/%s";
+    private final String bucketName;
+    private final S3Presigner s3Presigner;
+
+    public PresignedUrlProvier(
+            @Value("${cloud.s3.bucket}")
+            String bucketName,
+            S3Presigner s3Presigner) {
+        this.bucketName = bucketName;
+        this.s3Presigner = s3Presigner;
+    }
+
+    public String providePutObjectUrl(String fileName, String keyPattern) {
+
+        String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+        validateImageFile(extension);
+
+        String key = String.format(keyPattern, UUID.randomUUID());
+        String contentType = String.format(CONTENT_TYPE_PATTERN, extension.toLowerCase());
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType(contentType)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .putObjectRequest(putObjectRequest)
+                .signatureDuration(Duration.ofMinutes(3))
+                .build();
+
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(presignRequest);
+
+        return presignedRequest.url().toString();
+    }
+
+    private void validateImageFile(String extension) {
+        String imageFileRegex = "\\(jpeg|jpg|png|bmp\\)";
+        if (!extension.toLowerCase().matches(imageFileRegex)) {
+            throw new InvalidValueException(NOT_SUPPORTED_FILE_TYPE);
+        }
+    }
+
+}
---
+++ src/main/java/dev/steady/storage/exception/StorageErrorCode.java
@@ -0,0 +1,25 @@
+package dev.steady.storage.exception;
+
+import dev.steady.global.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum StorageErrorCode implements ErrorCode {
+
+    NOT_SUPPORTED_FILE_TYPE("ST01", "지원하지 않는 파일 유형입니다.");
+
+    private final String code;
+    private final String message;
+
+    @Override
+    public String code() {
+        return this.code;
+    }
+
+    @Override
+    public String message() {
+        return this.message;
+    }
+}
