3d36a1bd2be80f53934ad2bd67e63afa4f7e3c7e
na-yk <ninghao17@naver.com>
2023-11-24 11:40:39 +0900

    :wrench: AwsConfig 추가

---
+++ src/main/java/dev/steady/global/config/AwsConfig.java
@@ -0,0 +1,33 @@
+package dev.steady.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+public class AwsConfig {
+
+    @Value("${cloud.aws.region}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access_key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret_key}")
+    private String secretKey;
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        AwsCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .build();
+    }
+
+}
