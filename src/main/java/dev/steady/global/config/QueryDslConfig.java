4cf9bcc075a143d05b35b31de018d6a308261536
weonest <geonhee33@naver.com>
2023-12-06 19:31:05 +0900

    :sparkles: SQLTemplates 설정 추가

--- src/main/java/dev/steady/global/config/QueryDslConfig.java
+++ src/main/java/dev/steady/global/config/QueryDslConfig.java
@@ -2,6 +2,8 @@ package dev.steady.global.config;
 
 import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.sql.MySQLTemplates;
+import com.querydsl.sql.SQLTemplates;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,11 @@ public class QueryDslConfig {
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager em) {
         return new JPAQueryFactory(JPQLTemplates.DEFAULT, em);
+    }
+
+    @Bean
+    public SQLTemplates mysqlTemplates() {
+        return MySQLTemplates.builder().build();
     }
 
 }
